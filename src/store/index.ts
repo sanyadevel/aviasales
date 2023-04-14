d3f3e26726959e206bb805123a73a3aede3a67e8
Alex Besleaga <sanyadevel@gmail.com>
2023-04-15 00:20:27 +0300

    refactoring

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
-import { createStore } from "redux";
-
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import { rootReducer } from "./reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
